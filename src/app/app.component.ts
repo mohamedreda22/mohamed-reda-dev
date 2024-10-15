16d47100b4f0a783e97adcc677cb123af8fc6529
mohamedreda22 <73208810+mohamedreda22@users.noreply.github.com>
2024-10-15 16:48:22 +0300

    working with formating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,18 +34,102 @@ export class AppComponent {
   title = 'Mohamed Reda Dev';
 
   // Skills and Projects data
-  skills: string[] = ['JavaScript', 'React.js', 'HTML', 'CSS', 'Node.js', 'Git', 'Angular'];
-  projects = [
-    { name: 'Helwan Newsline System', description: 'University communication system using React.js', date: '2024' },
-    { name: 'My Valentine', description: 'Interactive web app built with React.js', date: '2023' },
-    { name: 'Restaurant Website', description: 'Showcasing restaurant services and menu', date: '2023' },
-  ];
+// Skills and Projects data
+skills = [
+  { name: 'JavaScript', icon: 'fab fa-js-square' },
+  { name: 'React.js', icon: 'fab fa-react' },
+  { name: 'Angular', icon: 'fab fa-angular' },
+  { name: 'TypeScript', icon: 'fab fa-js-square' },
+  { name: 'HTML', icon: 'fab fa-html5' },
+  { name: 'CSS', icon: 'fab fa-css3-alt' },
+  { name: 'Node.js', icon: 'fab fa-node-js' },
+  { name: 'Git', icon: 'fab fa-git-alt' },
+  { name: 'Python', icon: 'fab fa-python' },
+  { name: 'MongoDB', icon: 'fas fa-database' },
+  { name: 'SQL', icon: 'fas fa-database' },
+  { name: 'Bootstrap', icon: 'fab fa-bootstrap' },
+  { name: 'Sass', icon: 'fab fa-sass' },
+  { name: 'Jest', icon: 'fas fa-vial' },
+  { name: 'Agile', icon: 'fas fa-tasks' },
+  { name: 'Scrum', icon: 'fas fa-tasks' },
+  { name: 'CI/CD', icon: 'fas fa-code-branch' },
+  { name: 'REST API', icon: 'fas fa-server' },
+  { name: 'Docker', icon: 'fab fa-docker' },
+  { name: 'Linux', icon: 'fab fa-linux' },
+  { name: 'VS Code', icon: 'fas fa-code' },
+  { name: 'Postman', icon: 'fas fa-envelope' },
+  { name: 'Jira', icon: 'fab fa-jira' },
+  { name: 'Slack', icon: 'fab fa-slack' },
+  { name: 'MERN Stack', icon: 'fas fa-layer-group' }, 
+  { name: 'MEAN Stack', icon: 'fas fa-cubes' },
+  { name: 'Generative AI', icon: 'fas fa-robot' }, 
+  { name: 'Software Development', icon: 'fas fa-code' }, 
+  { name: 'Redux.js', icon: 'fas fa-exchange-alt' },
+  { name: 'Web Development', icon: 'fas fa-laptop-code' }, 
+  { name: 'Front-end Development', icon: 'fas fa-desktop' },
+  
+
+
+
+
+];
+projects = [
+  { 
+    name: 'Bistro Bliss Restaurant', 
+    description: `
+    Introducing Bistro Bliss, a full-stack web application designed to showcase my skills in both front-end and back-end development.<br>
+    <strong>Key Technologies:</strong>
+      <ul>
+        Front-End: Angular, HTML, CSS, TypeScript <br>
+        Back-End: Node.js, Express, MongoDB
+      </ul>
+      <strong>Features:</strong>
+      <ul>
+        RESTful API for CRUD operations <br>
+        Responsive Design for compatibility across devices<br>
+        User Authentication and Authorization
+      </ul>
+
+Check out the <a href="https://mohamedreda22.github.io/ODC-Final-project-Demo/" target="_blank" >GitHub repository</a> for more details.
+    `, 
+    link:"https://github.com/mohamedreda22/ODC-Final-project-Demo",
+    date: 'SEP-2024' ,
+    images: ['odc1.png', 'odc2.png', 'odc3.png','odc4.png', 'odc5.png', 'odc6.png','odc7.png', 'odc8.png','odc10.png', 'odc11.png'],
+    currentImage: 'odc1.png'
+  },
+  { 
+    name: 'Bistro Bliss Restaurant', 
+    description: `
+    Introducing Bistro Bliss, a full-stack web application designed to showcase my skills in both front-end and back-end development.<br>
+      
+    <strong>Key Technologies:</strong>
+      <ul>
+        Front-End: Angular, HTML, CSS, TypeScript <br>
+        Back-End: Node.js, Express, MongoDB
+      </ul>
+      <strong>Features:</strong>
+      <ul>
+        RESTful API for CRUD operations <br>
+        Responsive Design for compatibility across devices<br>
+        User Authentication and Authorization
+      </ul>
+
+Check out the <a href="https://mohamedreda22.github.io/ODC-Final-project-Demo/" target="_blank" >GitHub repository</a> for more details.
+    `, 
+    link:"https://github.com/mohamedreda22/ODC-Final-project-Demo",
+    date: 'SEP-2024' ,
+    images: ['odc1.png', 'odc2.png', 'odc3.png','odc4.png', 'odc5.png', 'odc6.png','odc7.png', 'odc8.png','odc10.png', 'odc11.png'],
+    currentImage: 'odc1.png'
+  },
+];
+
+
 
   // CV Download method
   downloadCV() {
     try {
       const link = document.createElement('a');
-      link.href = 'assets/MohamedReda_Resume_2024_ATS.docx'; 
+      link.href = 'MohamedReda_Resume_2024_ATS.docx'; 
       link.download = 'MohamedReda_Resume_2024_ATS.docx';
       link.click();
     } catch (error) {
@@ -53,7 +137,18 @@ export class AppComponent {
       alert('Download failed. Please try again later.');
     }
   }
-
+  constructor() {
+    this.startSlideshow();
+  }
+  startSlideshow() {
+    this.projects.forEach((project) => {
+      let currentIndex = 0;
+      setInterval(() => {
+        currentIndex = (currentIndex + 1) % project.images.length;
+        project.currentImage = project.images[currentIndex];
+      }, 1500); // Change image every 1000ms (1 second)
+    });
+  }
   // Contact form submission method
   onSubmit(contactForm: any) {
     if (contactForm.valid) {
